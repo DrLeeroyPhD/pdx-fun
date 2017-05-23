39ed391301ebd63b95a6fe467fa8fbe4c803f88b
Christopher Denny <dennyjchristopher@gmail.com>
2017-05-23 04:48:04 +0000

    Animations added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,3 +5,23 @@ $(".clickynav").click(function(){
         scrollTop: $(jump).offset().top - 80
     }, 600);
 });
+
+// Scroll Reveal Animations
+window.sr = ScrollReveal();
+sr.reveal('.catLogo', {
+    scale: '0.4',
+    easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)'
+});
+sr.reveal('#about', {
+    origin: 'left',
+    distance: '20%'
+});
+sr.reveal('#inventory', {
+    origin: 'right',
+    distance: '20%'
+});
+sr.reveal('#contact', {
+    origin: 'bottom',
+    distance: '20%'
+});
+
